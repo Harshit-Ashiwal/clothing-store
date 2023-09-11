58005011cdb5bc5d9ac70a0d1c36475b96d79470
unknown <harshitasiwal27@gmail.com>
2023-09-11 13:58:05 +0530

    image and background changes

--- src/Pages/ContactPage/ContactPage.jsx
+++ src/Pages/ContactPage/ContactPage.jsx
@@ -4,14 +4,14 @@ import React from "react";
 import "./ContactPage.style.scss";
 
 // images
-import Profile from "../../assets/profile.jpg";
+import Profile from "../../assets/profile.jpeg";
 
 const ContactPage = () => {
   return (
     <div className="contact-container">
       <h1>GET IN TOUCH</h1>
-      <a href="https://www.linkedin.com/in/hemant-ghatawal/" target="blank">
-        <h3 className="name">Hemant Saini - Web Developer</h3>
+      <a href="https://www.linkedin.com/in/harshit-ashiwal/" target="blank">
+        <h3 className="name">Harshit Ashiwal Web Developer</h3>
         <img className="profile-image" src={Profile} alt="Hemant" />
       </a>
     </div>
